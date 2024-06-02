c179c5fa42530b2d8ad8ab9ca1ed6d6d26c9cccb
rumi-mridha <rumamridha37@gmail.com>
2024-06-02 12:13:04 +0530

    add new  file

---
+++ basic_01/03_conversionOperation.js
@@ -0,0 +1,54 @@
+//let score = 33
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber=Number(score)
+///console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+let IsLoggedIn = "Ruma"
+let booleanIsLoggedIN = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIN);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ruma" => true
+let someNumber =33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log( typeof StringNumber);
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+ console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
