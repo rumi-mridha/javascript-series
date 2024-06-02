a709d84656b5ceb9b75bda4c7605952bb7cc70c5
rumi-mridha <rumamridha37@gmail.com>
2024-06-02 22:36:43 +0530

    add new file

---
+++ basic_01/06_string.js
@@ -0,0 +1,22 @@
+const name = "Ruma"
+const repocount = 50
+console.log(name +repocount+"great");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ruma')
+console.log(gameName.length);
+console.log(gameName[3]);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("r"));
+const newString =gameName.substring(0,3)
+console.log(newString);
+const anotherString = gameName.slice(-2,3)
+console.log(anotherString);
+const newStringone = "    Ruma"
+console.log(newStringone);
+console.log(newStringone.trim());
+const url =" https//:www.youtube.com"
+console.log(url.replace('ww','_'));
+console.log(url.includes('com'));
+console.log(gameName.split('_'));
